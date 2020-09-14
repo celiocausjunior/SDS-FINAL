4eb37260e9d3fc831d06e50ad01919d77f9a3e37
Celio Caus Junior <celiocausjunior@gmail.com>
2020-09-14 13:48:26 -0300

    Post Record

---
+++ backend/src/main/java/com/celiocausjunior/dspesquisa/services/RecordService.java
@@ -0,0 +1,40 @@
+package com.celiocausjunior.dspesquisa.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.celiocausjunior.dspesquisa.dto.RecordDTO;
+import com.celiocausjunior.dspesquisa.dto.RecordInsertDTO;
+import com.celiocausjunior.dspesquisa.entities.Game;
+import com.celiocausjunior.dspesquisa.entities.Record;
+import com.celiocausjunior.dspesquisa.repositories.GameRepository;
+import com.celiocausjunior.dspesquisa.repositories.RecordRepository;
+
+@Service
+public class RecordService {
+
+	@Autowired
+	private RecordRepository recordRepository;
+
+	@Autowired
+	private GameRepository gameRepository;
+
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+
+		Record entity = new Record();
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+
+		recordRepository.save(entity);
+		return new RecordDTO(entity);
+	}
+
+}
